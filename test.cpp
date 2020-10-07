2d1f158f5084c403c31b69ac8caaf8c2f9e46701
madv809 <62539108+madv809@users.noreply.github.com>
2020-10-07 21:10:55 +0700

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -9,8 +9,8 @@
 #include <math.h>
 
 using namespace std;
-const LL INF = 100000000009;
-pair <int, int> a[100001];
+const LL INF = 100000000000009;
+pair <int, int> a[200001];
 int n;
 
 bool cmp (pair <int, int> a, pair <int, int> b)
@@ -23,25 +23,23 @@ int main()
     //freopen("D:\\test.txt", "r", stdin);
     //freopen("D:\\test2.txt", "w", stdout);
     scanf("%d", &n);
-    for (int i = 1; i <= n; ++i) scanf("%d%d", a[i].first, a[i].second);
-    sort(a + 1, a + n + 1, cmp);
-    int j = n - 1;
-    LL sumx = 0, sumy = 0, ans = -INF, res = -INF;
-    while (j) a[++n] = a[j--];
+    for (int i = 0; i < n; ++i) scanf("%d%d", &a[i].first, &a[i].second);
+    sort(a, a + n, cmp);
+    LL sumx, sumy, ans, res = -INF;
     for (int i = 1; i <= n; ++i)
+    {
+        sumx = 0; sumy = 0, ans = -INF;
         for (int j = 0; j < n; ++j)
         {
-            sumx += a[i + j].first;
-            sumy += a[i + j].second;
-            if (sumx*sumx + sumy*sumy > ans) ans = sumx*sumx + sumy*sumy;
+            sumx += a[(i + j)%n].first;
+            sumy += a[(i + j)%n].second;
+            if (sumx*sumx + sumy*sumy >= ans) ans = sumx*sumx + sumy*sumy;
             else
             {
-                sumx = 0;
-                sumy = 0;
                 res = max(res, ans);
-                ans = -INF;
                 break;
             }
         }
+    }
     printf("%lli", res);
 }
