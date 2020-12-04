ea1d6f0756bd8579240ec313b9da86c4507083e5
madv809 <62539108+madv809@users.noreply.github.com>
2020-12-04 22:32:30 +0700

    Add files via upload

--- something.cpp
+++ something.cpp
@@ -23,14 +23,11 @@
 using namespace std;
 
 const int mxn = 2e5+5;
-vector<int> it[mxn], a, b, c;
+vector<int> a, b, c;
 int main()
 {
     //freopen("D:\\test.txt", "r", stdin);
     //freopen("D:\\test2.txt", "w", stdout);
-    //it[2].pb(4); it[3].pb(5);
-    //merge(it[2].begin(), it[2].end(), it[3].begin(), it[3].end(), it[1].begin());
-    //for (int i : it[1]) cout << i << ' ';
     a.pb(1); b.pb(2);
     merge(a.begin(), a.end(), b.begin(), b.end(), c.begin());
     for (int i : c) cout << i << ' ';
