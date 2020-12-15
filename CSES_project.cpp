06f0a4d768cedee252d2da45ff880e07f86ff66b
madv809 <62539108+madv809@users.noreply.github.com>
2020-12-15 17:04:04 +0700

    Update CSES_project.cpp

--- CSES_project.cpp
+++ CSES_project.cpp
@@ -7,8 +7,7 @@
 #include <math.h>
 #include <iomanip>
 #include <queue>
-#include <map>
-
+ 
 #define LL long long
 #define FOR(i, a, b) for (int i = a; i < b; ++i)
 #define REP(i, a, b) for (int i = a; i <= b; ++i)
@@ -20,126 +19,36 @@
 #define piL pair<int, LL>
 #define pLL pair<LL, LL>
 #define ar array
-
+ 
 using namespace std;
-const LL MOD = 1e9+7; const int BASE = 1e4;
-struct big
+ 
+const int mxn = 1e5 + 5;
+pii a[mxn]; int n;
+ 
+long double cal (long double t)
 {
-    int a[2304]; int n;
-    int &operator [] (int i) {return a[i];}
-    const int &operator [] (int i) const {return a[i];}
-    void resize (int x) {n = x; FOR(i, 0, n) a[i] = 0;}
-    void trim () {while (n > 1 && a[n-1] == 0) --n;}
-    void normalize()
+    long double mi, mx; mi = mx = a[1].Y*t + a[1].X;
+    REP(i, 1, n)
     {
-        for (int i = 0; i < n - 1; ++i)
-        {
-            a[i + 1] += a[i]/BASE;
-            a[i] %= BASE;
-        }
-        trim();
+        mi = min(mi, a[i].Y*t + a[i].X);
+        mx = max(mx, a[i].Y*t + a[i].X);
     }
-    void operator = (const int &x)
-    {
-        n = 1;
-        a[0] = x;
-    }
-    void operator = (const big &A)
-    {
-        n = A.n;
-        for (int i = 0; i < n; ++i) a[i] = A[i];
-    }
-};
-
-big operator + (const big &A, const big &B)
-{
-    big R;
-    if (A.n >= B.n)
-    {
-        R = A; ++R.n;
-        for (int i = 0; i < B.n; ++i) R[i] += B[i];
-    }
-    else
-    {
-        R = B; ++R.n;
-        for (int i = 0; i < A.n; ++i) R[i] += A[i];
-    }
-    R.normalize();
-    return R;
+    return (mx - mi);
 }
-
-big operator * (const big &A, const big &B)
-{
-    big R; R.resize(A.n + B.n);
-    for (int i = 0; i < A.n; ++i)
-        for (int j = 0; j < B.n; ++j)
-        {
-            R[i + j] += A[i]*B[j];
-            R[i + j + 1] += R[i + j]/BASE;
-            R[i + j]%=BASE;
-        }
-    R.normalize();
-    return R;
-}
-
-big operator * (const big &A, const int &x)
-{
-    big R; R.resize(A.n + 1);
-    for (int i = 0; i < A.n; ++i) R[i] = A[i]*x;
-    R.normalize();
-    return R;
-}
-
-iostream &operator >> (iostream &cin, big &A)
-{
-    string s;
-    cin >> s; int k = s.length();
-    A.resize(k/4 + 1);
-    for (int i = 0; i < k; ++i)
-    {
-        int x = (k - i - 1)/4;
-        A[x] = A[x]*10 + (s[i] - '0');
-    }
-    A.normalize();
-    return cin;
-}
-
-ostream &operator << (ostream &cout, const big &A)
-{
-    printf("%d",A[A.n - 1]);
-    for (int i = A.n - 2; i >= 0; --i) printf("%04d", A[i]);
-    return cout;
-}
-
-big f[10001], g[10001];
+ 
 int main()
 {
     //freopen("D:\\test.txt", "r", stdin);
     //freopen("D:\\test2.txt", "w", stdout);
-    string s; getline(cin,s); s = '@'+s;
-    f[0] = 1; g[0] = 1;
-    for (int i = 1; i < s.length(); ++i)
+    cin >> n;
+    REP(i, 1, n) scanf("%d", &a[i].X);
+    REP(i, 1, n) scanf("%d", &a[i].Y);
+    long double l = 0, r = 1e10;
+    REP(i, 1, 300)
     {
-        if (s[i] == '*')
-        {
-            g[i] = g[i-1]*3;
-            f[i] = f[i-1]*5 + g[i-1];
-        }
-        else if (s[i] == 'L')
-        {
-            g[i] = g[i-1];
-            f[i] = f[i-1]*2;
-        }
-        else if (s[i] == 'R')
-        {
-            g[i] = g[i-1];                             // L *  R
-            f[i] = f[i-1]*2 + g[i-1];                  // 2 12
-        }                                              // 1 3
-        else
-        {
-            f[i] = f[i-1];
-            g[i] = g[i-1];
-        }
+        long double m1 = l + (r - l)/3, m2 = r - (r - l)/3;
+        if (cal(m1) > cal(m2)) l = m1;
+        else r = m2;
     }
-    cout << f[s.length()-1];
+    cout << fixed << setprecision(3) << cal(l)/2;
 }
