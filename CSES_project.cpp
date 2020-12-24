f77aeb7432c19986f7dc34b84a5a769faecf2506
madv809 <62539108+madv809@users.noreply.github.com>
2020-12-24 15:26:03 +0700

    Update CSES_project.cpp

--- CSES_project.cpp
+++ CSES_project.cpp
@@ -7,7 +7,8 @@
 #include <math.h>
 #include <iomanip>
 #include <queue>
- 
+#include <map>
+
 #define LL long long
 #define FOR(i, a, b) for (int i = a; i < b; ++i)
 #define REP(i, a, b) for (int i = a; i <= b; ++i)
@@ -19,36 +20,50 @@
 #define piL pair<int, LL>
 #define pLL pair<LL, LL>
 #define ar array
- 
+
 using namespace std;
- 
-const int mxn = 1e5 + 5;
-pii a[mxn]; int n;
- 
-long double cal (long double t)
-{
-    long double mi, mx; mi = mx = a[1].Y*t + a[1].X;
-    REP(i, 1, n)
-    {
-        mi = min(mi, a[i].Y*t + a[i].X);
-        mx = max(mx, a[i].Y*t + a[i].X);
-    }
-    return (mx - mi);
-}
- 
+
+const int mxn = 2e6 + 5;
+const int INF = 1e9;
+LL f[mxn], g[mxn], a[mxn];
+
 int main()
 {
     //freopen("D:\\test.txt", "r", stdin);
     //freopen("D:\\test2.txt", "w", stdout);
-    cin >> n;
-    REP(i, 1, n) scanf("%d", &a[i].X);
-    REP(i, 1, n) scanf("%d", &a[i].Y);
-    long double l = 0, r = 1e10;
-    REP(i, 1, 300)
+    //freopen("angry.in", "r", stdin);
+    //freopen("angry.out", "w", stdout);
+    int n; cin >> n;
+    REP(i, 1, n) cin >> a[i];
+    sort(a + 1, a + n + 1);
+    int nn = 1;
+    REP(i, 2, n) if (a[i] != a[i - 1]) a[++nn] = a[i];
+    n = nn;
+    f[1] = 0;
+    REP(i, 2, n) if (a[i] - a[i - 1] > f[i - 1])
+        f[i] = a[i] - a[i - 1] + 1;
+    else if (a[i] - a[i - 1] <= f[i - 1])
+        f[i] = f[i - 1] + 1;
+
+    g[n] = 0;
+    for (int i = n - 1; i >= 1; --i) if (a[i + 1] - a[i] > g[i + 1])
+        g[i] = a[i + 1] - a[i] + 1;
+    else if (a[i + 1] - a[i] <= g[i + 1])
+        g[i] = g[i + 1] + 1;
+
+    double res = 2*INF;
+    int i = 1, j = n;
+    do
     {
-        long double m1 = l + (r - l)/3, m2 = r - (r - l)/3;
-        if (cal(m1) > cal(m2)) l = m1;
-        else r = m2;
-    }
-    cout << fixed << setprecision(3) << cal(l)/2;
+        double x = a[j] - a[i]; x /= 2;
+        double k = max(f[i], g[j]);
+        if (x < k) x += k - x;
+        res = min(res, x);
+        if (f[i + 1] < g[j - 1]) ++i;
+        else --j;
+    } while (i <= j);
+    cout << fixed << setprecision(1) << res;
+    /*REP(i, 1, n) cout << a[i] << ' '; cout << endl;
+    REP(i, 1, n) cout << f[i] << ' '; cout << endl;
+    REP(i, 1, n) cout << g[i] << ' ';*/
 }
